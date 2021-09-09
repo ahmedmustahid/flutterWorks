d8299e50c3396d1e8d83fb33649b04bfe96cd5c1
ahmedmustahid <einsteinium25@gmail.com>
2021-09-09 17:05:20 +0900

    amplify flutter 1

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,17 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "amplifyflutter1": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://oezvwurfs5evho5svd2kupl43q.appsync-api.ap-northeast-1.amazonaws.com/graphql",
+                    "region": "ap-northeast-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-kbcue32umvg2vf5knuivhhgdiu"
+                }
+            }
+        }
+    }
+}''';
